a49ce70049c6017438d247f37c3030f34e8d5376
snebotcifpfbmoll <snebot@cifpfbmoll.com>
2020-02-18 19:28:05 +0100

    comments

--- JSON/P01/js/main.js
+++ JSON/P01/js/main.js
@@ -2,16 +2,24 @@ function carregarJSON() {
     const json = '{"classe":{"@assignatura":"musica","professor":{"nom":"Alvaro Clemente","any_neixament":"1960","anys_experiencia":"35"},"alumnes":[{"alumne":{"@nom":"Freddie Mercury","any_neixament":"1946","nota_examen":"9","nota_practica":"Perfecte","descripcio":"Si practica pot cantar b\u00e9"}},{"alumne":{"@nom":"John Lennon","any_neixament":"1940","nota_examen":"8","nota_practica":"Notable","descripcio":"T\u00e9 bastanta imaginaci\u00f3"}},{"alumne":{"@nom":"Mark Knopfler","any_neixament":"1949","nota_examen":"9","nota_practica":"Notable","descripcio":"Comen\u00e7a a tocar la guitarra"}},{"alumne":{"@nom":"Justin Bieber","any_neixament":"1994","nota_examen":"0","nota_practica":"Insuficient","descripcio":"Sense comentaris"}},{"alumne":{"@nom":"Lemmy Kilmister","any_neixament":"1945","nota_examen":"3","nota_practica":"Notable","descripcio":"Jove molt sa."}}]}}';
     const objJson = JSON.parse(json);
 
+    // Obtenir element div (contenidor)
     contenidor = document.getElementById("contenidor");
+
+    // L'element assignatura que sera el titol
     assignatura = document.createElement("h1");
     assignatura.setAttribute("id", "nom_classe");
     assignatura.innerText = objJson.classe["@assignatura"];
-    profe = document.createElement("p");
+
+    // L'element profe sera un subtitol
+    profe = document.createElement("div");
     profe.innerHTML = "<h2>Professor</h2>";
-    profe.innerHTML += "Nom: " + objJson.classe.professor.nom + "</br>";
+    profe.innerHTML += "<p>Nom: " + objJson.classe.professor.nom + "</p>";
+
+    // L'element alumnes contindra tots els alumnes dins de la classe
     alumnes = document.createElement("div");
     alumnes.innerHTML += "<h2>Alumnes</h2>";
 
+    // Afegim tots els alumnes
     for (var i = 0; i < objJson.classe.alumnes.length; i ++) {
         objAlumne = objJson.classe.alumnes[i];
         alumne = document.createElement("p");
@@ -23,6 +31,7 @@ function carregarJSON() {
         alumnes.appendChild(alumne);
     }
 
+    // Afegim tots els elements al document
     contenidor.appendChild(assignatura);
     contenidor.appendChild(profe);
     contenidor.appendChild(alumnes);
